9e4687fb6801e9c7be566e96d397501f2b625e35
CorranHornet <texhex14@outlook.com>
2025-11-19 09:39:38 +0100

    added a menu

--- CLI/MenuHelper.cs
+++ CLI/MenuHelper.cs
@@ -15,34 +15,37 @@ namespace CLI
             bool isOn = true;
             while (isOn)
             {
-                Console.WriteLine("1. Setttings Menu");
-            Console.WriteLine("2. Display Note Repository");
-            Console.WriteLine("3. Recording Studio");
-            Console.WriteLine("Q. Quits the program");
-            string userSelected = Console.ReadLine();
+                Console.WriteLine("1. Settings Menu"); // fixed typo
+                Console.WriteLine("2. Display Note Repository");
+                Console.WriteLine("3. Recording Studio");
+                Console.WriteLine("Q. Quit the program"); // fixed typo
+                string userSelected = Console.ReadLine();
 
-            
-           
                 switch (userSelected)
                 {
                     case "1":
                         SettingsMenu();
                         break;
+
                     case "2":
                         RepositoryView();
-
                         break;
+
                     case "3":
                         RecordingStudio();
                         break;
-                    case "Q" or "q":
+
+                    case "Q":
+                    case "q":
                         isOn = false;
                         break;
+
                     default:
                         break;
                 }
             }
         }
+
         public void RecordingStudio()
         {
             bool isOn = true;
@@ -51,66 +54,72 @@ namespace CLI
                 Console.WriteLine("1. Start Recording");
                 Console.WriteLine("2. Stop Recording");
                 Console.WriteLine("3. Back to previous Menu");
+
                 string userSelected = Console.ReadLine();
+
                 switch (userSelected)
                 {
                     case "1":
                         break;
+
                     case "2":
                         break;
+
                     case "3":
                         Console.WriteLine("Back to prev menu");
-                        isOn=false;
+                        isOn = false;
                         break;
-
                 }
-                Console.WriteLine("This is shown when using break");
             }
         }
-        
+
         public void RepositoryView()
         {
             bool isOn = true;
             while (isOn)
             {
                 Console.WriteLine("Display Note Repository");
-                Console.WriteLine("1. Display Note Repositotry");
+                Console.WriteLine("1. Display Note Repository"); // fixed typo
                 Console.WriteLine("2. Back to previous menu ");
+
                 string userSelected = Console.ReadLine();
+
                 switch (userSelected)
                 {
                     case "1":
                         Console.WriteLine("Shows Repository");
-
                         break;
-                        
+
                     case "2":
                         isOn = false;
-
                         break;
+
                     default:
                         break;
                 }
             }
         }
-        public void SettingsMenu()
 
+        public void SettingsMenu()
         {
             bool isOn = true;
             while (isOn)
             {
                 Console.WriteLine("Settings Menu");
-                Console.WriteLine("1 Change Settings");
-                Console.WriteLine("2 Back to previous Menu");
+                Console.WriteLine("1. Change Settings"); // fixed typo
+                Console.WriteLine("2. Back to previous Menu");
+
                 string userSelected = Console.ReadLine();
 
                 switch (userSelected)
                 {
                     case "1":
                         break;
+
                     case "2":
-                        isOn=false;
+                        isOn = false;
                         break;
+
                     default:
                         break;
                 }
