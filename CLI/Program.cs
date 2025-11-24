eec8e64ab1ee5d62f431fc7a151c38cae61e6137
Nur Pelin <demirelpelin065@gmail.com>
2025-11-24 17:32:46 +0100

    Done with my UI part

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace CLI
+﻿using System;
+
+namespace CLI.UI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Säkerställ layout mot aktuell konsolstorlek
+            Layout.Initialize();
+
+            var ui = new UIManager();
+            ui.MainMenu();
         }
     }
 }
---
+++ CLI/UI/Class2.cs
@@ -0,0 +1,8 @@
+﻿namespace CLI.UI
+{
+    public static class Layout
+    {
+        public const int UiHeight = 20;
+        public const int BackgroundHeight = 20;
+    }
+}
